1ada6e23ff1e74bfe00ee9ca620c35e10c34c135
Maksym <example@gmail.com>
2023-08-06 15:03:48 +0200

    small changes

--- js/script.js
+++ js/script.js
@@ -12,22 +12,33 @@ closeElem.addEventListener('click', () => {
 
 const sidePanelText = document.querySelector('.sidepanel__text span');
 const sidePanelDivider = document.querySelector('.sidepanel__divider');
-const sidePanelLink = document.querySelectorAll('.sidepanel__img');
+const sidePanelLink = document.querySelectorAll('.sidepanel__link svg path');
 window.addEventListener('scroll', () => {
     const verticalScrollPx = window.scrollY;
-    if (verticalScrollPx < 225) {
-        sidePanelText.style.color = '#f5f5f5';
-        sidePanelDivider.style.backgroundColor = '#f5f5f5';
-        sidePanelLink.forEach(img => {
-            img.style.filter = '#f5f5f5';
-        });
-    } else {
-        sidePanelText.style.color = '#2A109E';
-        sidePanelDivider.style.backgroundColor = '#2A109E';
-        sidePanelLink.forEach(img => {
-            img.style.filter = '#2A109E';
-        });
+
+    function changeColor(scrollSize, item) {
+        if (verticalScrollPx >= scrollSize) {
+            item.classList.add('dark');
+        } else {
+            item.classList.remove('dark');
+        }
     }
+    changeColor(225, sidePanelText);
+    changeColor(325, sidePanelDivider);
+    sidePanelLink.forEach(el => changeColor(425, el));
+    // if (verticalScrollPx < 225) {
+    //     sidePanelText.style.color = '#f5f5f5';
+    //     sidePanelDivider.style.backgroundColor = '#f5f5f5';
+    //     sidePanelLink.forEach(img => {
+    //         img.style.filter = '#f5f5f5';
+    //     });
+    // } else if (verticalScrollPx >= 225) {
+    //     sidePanelText.style.color = '#2A109E';
+    // } else if (verticalScrollPx >= 325) {
+    //     sidePanelDivider.style.backgroundColor = '#2A109E';
+    // } else if (verticalScrollPx >= 425) {
+    //     sidePanelLink.forEach(link => link.style.fill = '#2A109E');
+    // }
 });
 
 const counters = document.querySelectorAll('.skills__percentage-counter'),
