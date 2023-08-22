9a45bc10ad7371c00bacc990aa2fe2ac86de167e
Maksym <example@gmail.com>
2023-08-22 15:19:43 +0200

    add project pulse

---
+++ pulse/js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        dots: true,
+        arrows: false
+      }
+    }, ]
+  });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Proszę wprowadzić imię",
+        phone: "Proszę wprowadzić swój numer telefonu",
+        email: {
+          required: "Proszę wprowadzić swojego maila",
+          email: "Adres email ma być w postaci name@domain.com"
+        }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('input[name=phone]').mask("+48 999-999-999")
+
+});
